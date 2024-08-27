3aea287e5242fc62c52e25925cc1d81e715e5c88
Arsh <pawar.arsh1333@gmail.com>
2024-08-27 19:43:35 +0530

    initial

--- src/script.js
+++ src/script.js
@@ -21,12 +21,10 @@ const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
 
-// "Show the dialog" button opens the dialog modally
 btn.addEventListener("click", () => {
   dialog.showModal();
 });
 
-// "Close" button closes the dialog
 closeButton.addEventListener("click", () => {
   dialog.close();
 });
